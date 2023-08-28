18a537a1f1b167ad42986636e014e8f00aac17a4
sbstzuluaga1111 <sbstzuluagajob@gmail.com>
2023-08-28 13:26:03 -0500

    V1

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,50 @@
-import React from 'react';
-import './App.css';
-import { useState ,useEffect } from 'react';
-import buscadorIcono from './buscador.svg';
-import TarjetaPelicula from './TarjetaPelicula';
+import React from "react";
+import "./App.css";
+import { useState, useEffect } from "react";
+import buscadorIcono from "./buscador.svg";
+import TarjetaPelicula from "./TarjetaPelicula";
 
-const API_URL = 'http://www.omdbapi.com?apikey=991d53a'
+const API_URL = "http://www.omdbapi.com?apikey=991d53a";
 
 const App = () => {
-    const [movies, setMovies]= useState([])
-    const [busquedaParam, setBusquedaParam] = useState('')
-    const buscadorCine = async (titulo) => {
-        const response = await fetch(`${API_URL}&s=${titulo}`)
-        const data = await response.json();
-        setMovies(data.Search);
-    }
-    useEffect(()=>{
-        buscadorCine('Blue Beetle');
-    },[])
-    return (
-        <div className='app'>
-           <h1>Joder, en efecto es Cine</h1> 
-           <div className='busqueda'>
-                <input placeholder='Buscar pelicula . . .'
-                value={busquedaParam}
-                onChange = {(e) => setBusquedaParam(e.target.value)}
-                />
-                <img
-                src={buscadorIcono}
-                alt = 'Buscador'
-                onClick={() => buscadorCine(busquedaParam)}
-                />
-           </div>
-           {movies?.length >0?(
-            <div className='contenedor'>
-                {
-                    movies.map((movie)=>(
-                        <TarjetaPelicula movie1={movie}/>
-                    ))
-                }
-                </div>
-           ): (
-                <div className='empty'>
-                    <h2>No hay sistema para esta pelicula</h2>
-                </div>
-           )
-           }
-            
-               
-                
+  const [movies, setMovies] = useState([]);
+  const [busquedaParam, setBusquedaParam] = useState("");
+  const buscadorCine = async (titulo) => {
+    const response = await fetch(`${API_URL}&s=${titulo}`);
+    const data = await response.json();
+    setMovies(data.Search);
+  };
+  useEffect(() => {
+    buscadorCine("");
+  }, []);
+  return (
+    <div className="app">
+      <h1>Joder, en efecto es Cine</h1>
+      <div className="busqueda">
+        <input
+          placeholder="Buscar pelicula . . ."
+          value={busquedaParam}
+          onChange={(e) => setBusquedaParam(e.target.value)}
+        />
+        <img
+          src={buscadorIcono}
+          alt="Buscador"
+          onClick={() => buscadorCine(busquedaParam)}
+        />
+      </div>
+      {movies?.length > 0 ? (
+        <div className="contenedor">
+          {movies.map((movie) => (
+            <TarjetaPelicula movie1={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No hay sistema para esta pelicula</h2>
+        </div>
+      )}
+    </div>
+  );
+};
 
-            </div>
-        
-    )
-}
-
-export default App
+export default App;
